2d941a7a460fcfeeb50448b61d8b7821f4687533
VadimLitau <vadimlitau@yandex.ru>
2023-09-13 16:02:09 +0600

    фыфы

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
       <Helmet
         title="My Titles"
       >
-        <meta name="keywords" content='печать на футболках, санкт-петербург, недорого, на заказ, цена, от 1 шт, срочный, заказать, хороший, сделать, стоимость, доставка, быстрый, качественный, черный, оверсайз, белый, онлайн, спортивный, свой дизайн, конструктор, создать макет, нанесение, собственный, толстовка, худи, студия, услуги, каталог, а3, а4, одежда, свитшот' data-react-helmet="true"/>
-        <meta name="description" content="Helmet application" data-react-helmet="true"/>
-        <meta property="og:image" content="https://sun9-77.userapi.com/impg/r3SRF7rtra4wl-3EmEgVqIRaaGNbjeO6q9ufUw/-yeDgKpu2CQ.jpg?size=500x500&quality=95&sign=d7fc90ef8c432358c10c8b1e16b4945f&type=album" data-react-helmet="true"/>
+        
         </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
