f544f742d04e27b121643a7ba78c856578284744
VadimLitau <vadimlitau@yandex.ru>
2023-09-13 15:41:43 +0600

    add helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Helmet from "react-helmet";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Helmet
+        title="My Title"
+        meta={[{ name: "description", content: "Helmet application" },{
+          name: 'keywords',
+          content:
+            'печать на футболках, санкт-петербург, недорого, на заказ, цена, от 1 шт, срочный, заказать, хороший, сделать, стоимость, доставка, быстрый, качественный, черный, оверсайз, белый, онлайн, спортивный, свой дизайн, конструктор, создать макет, нанесение, собственный, толстовка, худи, студия, услуги, каталог, а3, а4, одежда, свитшот',
+        },
+        {
+          property: 'og:image',
+          content:
+            'https://sun9-77.userapi.com/impg/r3SRF7rtra4wl-3EmEgVqIRaaGNbjeO6q9ufUw/-yeDgKpu2CQ.jpg?size=500x500&quality=95&sign=d7fc90ef8c432358c10c8b1e16b4945f&type=album',
+        },
+        {
+          property: 'og:title',
+          content:
+            'Печать на футболках заказать в Санкт-Петербурге цена от 1 штуки в Studio Pinhead',
+        },
+        {
+          property: 'og:url',
+          content: 'https://studio.pnhd.ru/',
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },]}
+        link={[
+          {
+            rel: "apple-touch-icon",
+            href: "http://mysite.com/img/apple-touch-icon-57x57.png",
+          },
+          {
+            rel: "apple-touch-icon",
+            sizes: "72x72",
+            href: "http://mysite.com/img/apple-touch-icon-72x72.png",
+          },
+        ]}
+        base={{ href: "http://mysite.com/" }}
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
