9c499e02f4f2c5e167ddf182929db28f9de85638
VadimLitau <vadimlitau@yandex.ru>
2023-09-13 15:50:29 +0600

    sas

--- src/App.js
+++ src/App.js
@@ -7,16 +7,12 @@ function App() {
     <div className="App">
       <Helmet
         title="My Title"
-        meta={[{ name: "description", content: "Helmet application" },{
+        meta={[{
           name: 'keywords',
           content:
             'печать на футболках, санкт-петербург, недорого, на заказ, цена, от 1 шт, срочный, заказать, хороший, сделать, стоимость, доставка, быстрый, качественный, черный, оверсайз, белый, онлайн, спортивный, свой дизайн, конструктор, создать макет, нанесение, собственный, толстовка, худи, студия, услуги, каталог, а3, а4, одежда, свитшот',
         },
-        {
-          property: 'og:image',
-          content:
-            'https://sun9-77.userapi.com/impg/r3SRF7rtra4wl-3EmEgVqIRaaGNbjeO6q9ufUw/-yeDgKpu2CQ.jpg?size=500x500&quality=95&sign=d7fc90ef8c432358c10c8b1e16b4945f&type=album',
-        },
+        
         {
           property: 'og:title',
           content:
@@ -42,7 +38,10 @@ function App() {
           },
         ]}
         base={{ href: "http://mysite.com/" }}
-      />
+      >
+        <meta name="description" content="Helmet application" data-react-helmet="true"/>
+        <meta property="og:image" content="https://sun9-77.userapi.com/impg/r3SRF7rtra4wl-3EmEgVqIRaaGNbjeO6q9ufUw/-yeDgKpu2CQ.jpg?size=500x500&quality=95&sign=d7fc90ef8c432358c10c8b1e16b4945f&type=album" data-react-helmet="true"/>
+        </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
